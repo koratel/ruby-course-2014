a0b4eb1c8f4058db2eec92ca36b9e578d4780480
Michal Lowicki <i258350@Lab107-01>
2014-10-13 15:30:42 +0200

    [#4] completed article class

--- article.rb
+++ article.rb
@@ -2,6 +2,42 @@ class Article
   def initialize(title, content, author="")
     @title, @content, @author = title, content, author
     @likes = @dislikes = 0
+    @created_at = Time.now
   end
 
+  def like!
+    @likes += 1
+  end
+
+  def dislike!
+    @dislikes += 1
+  end
+
+  def positive_votes
+    @likes - @dislikes
+  end
+
+  def votes
+    @likes + @dislikes
+  end
+
+  def shortened_to(limit)
+    if limit >= @content.length 
+      @content
+    else 
+      @content[0..limit-4] << "..."
+    end
+  end
+
+  def include?(pattern)
+    @content.include?(pattern)
+  end
+
+  def words
+    @content.split
+  end
+
+  def distinct_words
+    words.uniq!
+  end
 end

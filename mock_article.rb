884dba25b2ffe32d2a677bcd27b5b73c0be7068c
Michal Lowicki <milo.elessar@gmail.com>
2014-11-30 14:59:40 +0100

    [#16] Moved methods from MockArticle class to bla-bla gem

--- mock_article.rb
+++ mock_article.rb
@@ -1,38 +1,14 @@
 require_relative 'article'
-require_relative 'article_manager'
+require 'bla-bla'
 
 class MockArticle < Article
+
   def random_number
-    rand(100)
-  end
-
-  WORD_LIST = [
-    "zero", "one", "two", "three", "four",
-    "five", "six", "seven", "eight", "nine"
-  ]
-
-  def random_words(n)
-    WORD_LIST.sample(n)
-  end
-
-  def generate_author
-    random_words(2).collect { |word| word.capitalize }.join(" ")
-  end
-
-  def generate_title
-    random_words(rand(4)+1).join(" ").capitalize
-  end
-
-  def generate_sentence
-    random_words(rand(6)+5).join(" ").capitalize << "."
-  end
-
-  def generate_content
-    Array.new(rand(5)+1).collect!{ generate_sentence }.join(" ")
+    rand(101)
   end
 
   def initialize
-    super(generate_title, generate_content, generate_author)
+    super(BlaBla.generate_title, BlaBla.generate_content, BlaBla.generate_author)
     @likes, @dislikes = random_number, random_number
   end
 end
--- spec/unit/mock_article_spec.rb
+++ spec/unit/mock_article_spec.rb
@@ -5,31 +5,9 @@ require 'test/unit'
 class MockArticleSpec < Test::Unit::TestCase
   def setup
     @mock_article = MockArticle.new
-    @word_list = MockArticle::WORD_LIST
   end
 
   def test_random_number
     assert(@mock_article.random_number.between?(0, 100))
-  end
-
-  def test_random_words
-    assert_equal(10, @mock_article.random_words(10).size)
-    assert(@mock_article.random_words(10).all? { |word| @word_list.include?(word) })
-  end
-
-  def test_generate_author
-    assert(/\A[A-Z][a-z]* [A-Z][a-z]*\Z/ === @mock_article.generate_author)
-  end
-
-  def test_generate_title
-    assert(/\A[A-Z][a-z]*( [a-z]+){,4}\Z/ === @mock_article.generate_title)
-  end
-
-  def test_generate_sentence
-    assert(/\A[A-Z][a-z]*( [a-z]+){,9}.\Z/ === @mock_article.generate_sentence)
-  end
-
-  def test_generate_content
-    assert(/\A[A-Z][a-z]*( [a-z]+){,9}.( [A-Z][a-z]*( [a-z]+){,9}.){,4}\Z/ === @mock_article.generate_content)
   end
 end
